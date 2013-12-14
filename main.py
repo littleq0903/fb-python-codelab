bf9c658cdd0521acc89c9ef4f9645c117058373b
LittleQ <littleq0903@gmail.com>
2013-12-15 02:27:30 +0800

    [AUTO HEROKU DEPLOY]

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ Author: Colin Su <littleq0903@gmail.com>
 """
 from bottle import Bottle, run, template
 from bottle import static_file
+from bottle import request
 from bottle import jinja2_template
 import facebook
 
 import os
+import json
 
 # setup these environment variable by adding "heroku.config"
 # - FACEBOOK_APP_ID
@@ -27,3 +29,15 @@ def login():
     }
     return jinja2_template('index.html', **template_data)
 
+# Checkpoint 1
+@app.route('/cp1')
+def cp1():
+    cookies = request.cookies
+    fb_user = facebook.get_user_from_cookie(cookies, FACEBOOK_APP_ID, FACEBOOK_SECRET)
+
+    fb_access_token = fb_user["access_token"]
+    fb_graph = facebook.GraphAPI(fb_access_token)
+    profile = graph.get_object("me")
+
+    return json.dumps(profile)
+    
