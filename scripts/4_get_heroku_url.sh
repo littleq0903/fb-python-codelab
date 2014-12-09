397fd364e7941a250f0040514d706889fd2c7d68
LittleQ <littleq0903@gmail.com>
2014-12-09 22:45:39 +0800

    updated get heroku url script

--- scripts/4_get_heroku_url.sh
+++ scripts/4_get_heroku_url.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-heroku apps:info | grep -Eo "http://[[:alnum:]\.-]*/"
+parseurl () {
+    local input=$(cat)
+    local regex="Web URL:\ +(https://[[:alnum:]\.\-]*/)"
+    [[ $input =~ $regex ]]
+    echo ${BASH_REMATCH[1]}
+}
+heroku apps:info | parseurl
